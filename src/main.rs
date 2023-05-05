cec0ebbf464833126958ca53a65a038e9284ec3d
nightisyang <shenyangc86@gmail.com>
2023-05-05 22:14:21 +0800

    added functionality to add names, save  to file

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,5 @@
+use std::fs::File;
+use std::io::Write;
 use std::{ fs, io, env };
 use rand::Rng;
 use std::thread;
@@ -13,7 +15,7 @@ fn main() {
     // Construct the path to `names.txt` in the same directory as the executable
     let names_path = exe_dir.join("names.txt");
 
-    let content = fs::read_to_string(names_path).unwrap_or_default();
+    let content = fs::read_to_string(&names_path).unwrap_or_default();
 
     let mut names: Vec<String> = content
         .lines()
@@ -21,15 +23,10 @@ fn main() {
         .map(|line| line.to_string())
         .collect();
 
-    let array_len = &names.len();
-
     print!("\x1B[2J\x1B[1;1H");
     println!("Original sequence:");
-    for (index, name) in names.iter().enumerate() {
-        println!("{}. {}", index + 1, name);
-    }
-
-    println!("Type s to shuffle, x to quit, or c to choose");
+    loop_over_aray(&names);
+    print_instructions();
     let mut input_string = String::new();
 
     while input_string.trim() != "x" {
@@ -38,8 +35,8 @@ fn main() {
 
         // shuffle
         if input_string.trim() == "s" {
-            shuffle_names(&mut names, *array_len);
-            println!("Type s to shuffle, x to quit, or c to choose");
+            shuffle_names(&mut names);
+            print_instructions();
         }
 
         if input_string.trim() == "c" {
@@ -48,19 +45,31 @@ fn main() {
             choose_name(&mut names, 100, false);
             choose_name(&mut names, 250, true);
         }
+
+        if input_string.trim().starts_with("a") {
+            let new_name = input_string.trim_start_matches("a").trim().to_string();
+            let file_path = &names_path.to_str().expect("Failed to convert Path to str");
+
+            push_name_to_array(&mut names, new_name);
+            save_to_file(&names, file_path).unwrap();
+            print!("\x1B[2J\x1B[1;1H");
+            println!("Current Names:");
+            loop_over_aray(&names);
+            print_instructions();
+        }
     }
 
     println!("Goodbye!")
 }
 
-fn shuffle_names(names: &mut Vec<String>, array_len: usize) {
+fn shuffle_names(names: &mut Vec<String>) {
     // This will clear the screen and put the cursor at first row & first col of the screen.
     print!("\x1B[2J\x1B[1;1H");
 
-    for i in 0..array_len {
+    for i in 0..names.len() {
         let mut rng = rand::thread_rng();
         let range_start: usize = 0;
-        let range_end: usize = array_len;
+        let range_end: usize = names.len();
 
         let random_number: usize = rng.gen_range(range_start..range_end);
 
@@ -92,11 +101,34 @@ fn choose_name(names: &Vec<String>, time_ms: u64, choose: bool) {
 
         if choose && i == chosen_index {
             println!("Congratulation {}!", names[chosen_index]);
-
-            println!("Type s to shuffle, x to quit, or c to re-choose");
+            print_instructions();
             return;
         }
 
         thread::sleep(Duration::from_millis(time_ms));
     }
+}
+
+fn print_instructions() {
+    println!("Type s to shuffle, x to quit, c to choose, a <new_name> to add name");
+}
+
+fn loop_over_aray(names: &Vec<String>) {
+    for (index, name) in names.iter().enumerate() {
+        println!("{}. {}", index + 1, name);
+    }
+}
+
+fn push_name_to_array(names: &mut Vec<String>, new_name: String) {
+    names.push(new_name)
+}
+
+fn save_to_file(names: &Vec<String>, file_path: &str) -> std::io::Result<()> {
+    let mut file = File::create(file_path)?;
+
+    for name in names {
+        writeln!(file, "{}", name)?;
+    }
+
+    Ok(())
 }
